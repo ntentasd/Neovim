dd3a09e00ed81e46dd60f2821f452a2c2f2cb089
ntentasd <dimitrisntentas2002@gmail.com>
2024-08-16 19:30:10 +0300

    [ADDED]: Custom configurations

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,93 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+        "gopls",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = 'rust',
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^4",
+    ft = {"rust"},
+    dependencies = "neovim/nvim-lspconfig",
+    config = function ()
+      require "custom.configs.rustaceanvim"
+    end
+  },
+  {
+    "mfussenegger/nvim-dap",
+    init = function ()
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "dreamsofcode-io/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+      require("core.utils").load_mappings("dap_go")
+    end
+  },
+  {
+    "saecki/crates.nvim",
+    ft = {"rust", "toml"},
+    config = function (_, opts)
+      local crates = require('crates')
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "plugins.configs.cmp"
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opt = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "olexsmir/gopher.nvim",
+    ft = "go",
+    config = function(_, opts)
+      require("gopher").setup(opts)
+      require("core.utils").load_mappings("gopher")
+    end,
+    build = function()
+      vim.cmd [[silent! GoInstallDeps]]
+    end,
+  },
+  {
+    "wakatime/vim-wakatime",
+    lazy = false,
+  },
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  }
+}
+return plugins
