acfd426d441c0353089c6cfd92d9bbf0383ede9c
Dimitrios Ntentas <dimitrisntentas2002@gmail.com>
2024-09-07 20:50:21 +0300

    Added some plugins

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -47,3 +47,18 @@ lspconfig.clangd.setup {
   end,
   capabilities = capabilities,
 }
+
+lspconfig.rust_analyzer.setup {
+  on_attach = function(client, bufnr)
+    if client.server_capabilities.documentFormattingProvider then
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
+    on_attach(client, bufnr)
+  end,
+  capabilities = capabilities,
+}
